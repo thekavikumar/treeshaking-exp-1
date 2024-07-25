8926d9168cd74e2b4cf624a8eefb8f708ad7680a
Kavikumar M <kavikumarceo@gmail.com>
2024-07-25 13:55:29 +0530

    tree shaking donne

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function About() {
+  return (
+    <div className="about">
+      <h1>Rendering the About Component</h1>
+    </div>
+  );
+}
+
+export function Contact() {
+  return (
+    <div className="contact">
+      <h1>Rendering the contact component</h1>
+    </div>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { About } from './About';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <h1>Viewing App Component</h1>
+      <About />
     </div>
   );
 }
